b61b627adfbbd76d11a64de39cf0cffe20cc50d7
Carlos Frias <carlos.a.frias@gmail.com>
2019-09-26 23:54:43 -0300

    Add API boilerplate

---
+++ apps/api/src/index.ts
@@ -0,0 +1,37 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: "Something",
+    author: "Carlos",
+  },
+  {
+    title: "another",
+    author: "Charles",
+  },
+];
+
+const resolvers = {
+  Query: {
+    books() {
+      return books;
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
