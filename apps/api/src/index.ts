b1d90c30e03cf5f6e207b8eff876593fb47435fd
Carlos Frias <carlos.a.frias@gmail.com>
2019-09-28 20:23:17 -0300

    Add neo4j connection

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,3 +1,8 @@
+// Environment variables from `.env` files
+require("dotenv").config();
+
+import Neo4j from "neo4j-driver";
+
 import { ApolloServer, gql } from "apollo-server";
 
 const typeDefs = gql`
@@ -33,5 +38,22 @@ const resolvers = {
 const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
+  const driver = Neo4j.driver(
+    process.env.DB_URI,
+    Neo4j.auth.basic(process.env.DB_USER, process.env.DB_PASS),
+  );
+  const session = driver.session();
+
+  session
+    .run("MATCH (tom {name: $name}) RETURN tom", { name: "Tom Hanks" })
+    .then(result => {
+      session.close();
+      driver.close();
+      console.log(result.records[0].get(0));
+    })
+    .catch(err => {
+      console.error(err);
+    });
+
   console.log(`Server ready at ${url}`);
 });
